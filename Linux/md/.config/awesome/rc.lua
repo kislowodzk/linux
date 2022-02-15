e97e79e7dbc73acc2f3c1cc280cfbf1544d26e72
kislowodzk <kislowodzk@gmail.com>
2022-02-15 17:38:12 +0100

    disable tag switch by scrolling on desktop

--- Linux/md/.config/awesome/rc.lua
+++ Linux/md/.config/awesome/rc.lua
@@ -267,11 +267,12 @@ end)
 
 -- {{{ Mouse bindings
 root.buttons(gears.table.join(
-    awful.button({ }, 3, function () mymainmenu:toggle() end),
-    awful.button({ }, 4, awful.tag.viewnext),
-    awful.button({ }, 5, awful.tag.viewprev)
+    awful.button({ }, 3, function () mymainmenu:toggle() end)
+    -- awful.button({ }, 4, awful.tag.viewnext),
+    -- awful.button({ }, 5, awful.tag.viewprev)
 ))
 -- }}}
+
 
 -- {{{ Key bindings
 globalkeys = gears.table.join(
