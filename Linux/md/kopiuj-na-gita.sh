1293916fde8fef2dc752610478a2c41cc9a7484c
kislowodzk <kislowodzk@gmail.com>
2022-02-16 02:47:15 +0100

    Update kopiuj-na-gita.sh

--- Linux/md/kopiuj-na-gita.sh
+++ Linux/md/kopiuj-na-gita.sh
@@ -6,3 +6,4 @@ cp ~/.config/alacritty/alacritty.yml ~/Dokumenty/GitHub/linux/Linux/md/.config/a
 cp ~/.config/nvim/init.vim ~/Dokumenty/GitHub/linux/Linux/md/.config/nvim/
 cp ~/.kot ~/Dokumenty/GitHub/linux/Linux/md/
 cp ~/kopiuj-na-gita.sh ~/Dokumenty/GitHub/linux/Linux/md/
+
