791c9940fc51311f951762f1793a8a88e63797b4
kislowodzk <kislowodzk@gmail.com>
2022-02-22 22:25:38 +0100

    pa pa spacevim

--- Linux/md/.vimrc
+++ Linux/md/.vimrc
@@ -23,6 +23,7 @@ nnoremap <C-l> l
 set display+=lastline
 set backspace=indent,eol,start
 set scrolloff=8
+nnoremap <space> :w<cr>
 
 " Pozazuj białe znaki
 set listchars=tab:→\ ,space:·,nbsp:␣,trail:•,eol:¶
--- Linux/md/kopiuj-na-gita.sh
+++ Linux/md/kopiuj-na-gita.sh
@@ -3,8 +3,7 @@ cp ~/.vimrc ~/Dokumenty/GitHub/linux/Linux/md/
 cp ~/.bashrc ~/Dokumenty/GitHub/linux/Linux/md/
 cp ~/.config/awesome/rc.lua ~/Dokumenty/GitHub/linux/Linux/md/.config/awesome/
 cp ~/.config/alacritty/alacritty.yml ~/Dokumenty/GitHub/linux/Linux/md/.config/alacritty/
-cp -r ~/.config/nvim_backup ~/Dokumenty/GitHub/linux/Linux/md/.config/
-cp ~/.SpaceVim/init.vim ~/Dokumenty/GitHub/linux/Linux/md/init.vim_dla_space_vim.txt
+cp ~/.config/nvim/init.vim ~/Dokumenty/GitHub/linux/Linux/md/.config/nvim/
 cp ~/.kot ~/Dokumenty/GitHub/linux/Linux/md/
 cp ~/kopiuj-na-gita.sh ~/Dokumenty/GitHub/linux/Linux/md/
 cp ~/.config/ranger/rc.conf ~/Dokumenty/GitHub/linux/Linux/md/.config/ranger
