60aeafb8bb4e3c14f77b654917f018a0be25640c
kislowodzk <kislowodzk@gmail.com>
2022-03-08 13:57:27 +0100

    imwheel

---
+++ Linux/md/.kopiuj-na-gita.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cp ~/.vimrc ~/Dokumenty/GitHub/linux/Linux/md/
+cp ~/.bashrc ~/Dokumenty/GitHub/linux/Linux/md/
+cp ~/.config/awesome/rc.lua ~/Dokumenty/GitHub/linux/Linux/md/.config/awesome/
+cp ~/.config/alacritty/alacritty.yml ~/Dokumenty/GitHub/linux/Linux/md/.config/alacritty/
+cp ~/.config/nvim/init.vim ~/Dokumenty/GitHub/linux/Linux/md/.config/nvim/
+cp ~/.kopiuj-na-gita.sh ~/Dokumenty/GitHub/linux/Linux/md/
+cp ~/.mysz.sh ~/Dokumenty/GitHub/linux/Linux/md/
+cp ~/.config/ranger/rc.conf ~/Dokumenty/GitHub/linux/Linux/md/.config/ranger
+cp ~/.zshrc ~/Dokumenty/GitHub/linux/Linux/md/
+cp ~/.fonts/* ~/Dokumenty/GitHub/linux/Linux/md/.fonts
+cp ~/.tapeta/* ~/Dokumenty/GitHub/linux/Linux/md/.tapeta
+cp ~/.startup_zsh/* ~/Dokumenty/GitHub/linux/Linux/md/.startup_zsh
+cp ~/Notatki/Informatyka/vim_useful_comands.txt ~/Dokumenty/GitHub/linux/Linux/vim_useful_comands.txt
+
