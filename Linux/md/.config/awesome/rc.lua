a2850e5f37cd797e54585caa0b78c46880735a56
kislowodzk <kislowodzk@gmail.com>
2022-03-10 01:57:50 +0100

    Update rc.lua

--- Linux/md/.config/awesome/rc.lua
+++ Linux/md/.config/awesome/rc.lua
@@ -442,16 +442,16 @@ globalkeys = gears.table.join(
     --awful.key({ modkey },            "r",     function () awful.screen.focused().mypromptbox:run() end,
       --        {description = "run prompt", group = "launcher"}),
 
-    awful.key({ modkey }, "x",
-              function ()
-                  awful.prompt.run {
-                    prompt       = "Run Lua code: ",
-                    textbox      = awful.screen.focused().mypromptbox.widget,
-                    exe_callback = awful.util.eval,
-                    history_path = awful.util.get_cache_dir() .. "/history_eval"
-                  }
-              end,
-              {description = "lua execute prompt", group = "awesome"}),
+    -- awful.key({ modkey }, "x",
+              -- function ()
+                  -- awful.prompt.run {
+                    -- prompt       = "Run Lua code: ",
+                    -- textbox      = awful.screen.focused().mypromptbox.widget,
+                    -- exe_callback = awful.util.eval,
+                    -- history_path = awful.util.get_cache_dir() .. "/history_eval"
+                  -- }
+              -- end,
+              -- {description = "lua execute prompt", group = "awesome"}),
     -- Menubar
     awful.key({ modkey }, "q", function() menubar.show() end,
               {description = "show the menubar", group = "launcher"})
@@ -466,7 +466,7 @@ clientkeys = gears.table.join(
         {description = "toggle fullscreen", group = "client"}),
     awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end,
               {description = "close", group = "client"}),
-    awful.key({ modkey,    }, "z",      function (c) c:kill()                         end,
+    awful.key({ modkey,    }, "x",      function (c) c:kill()                         end,
               {description = "close", group = "client"}),
     awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ,
               {description = "toggle floating", group = "client"}),
