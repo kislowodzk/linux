3db9bae91e843001fbc9cdc6f4df18063d1845d3
kislowodzk <kislowodzk@gmail.com>
2022-05-23 00:18:58 +0200

    Update kopiuj-na-gita.sh

--- Linux/md/skrypty/kopiuj-na-gita.sh
+++ Linux/md/skrypty/kopiuj-na-gita.sh
@@ -8,7 +8,7 @@ cp ~/.config/ranger/rc.conf ~/Dokumenty/linux/Linux/md/.config/ranger
 cp ~/.zshrc ~/Dokumenty/linux/Linux/md/
 cp ~/.fonts/* ~/Dokumenty/linux/Linux/md/.fonts
 cp ~/.tapeta/* ~/Dokumenty/linux/Linux/md/.tapeta
-cp ~/.config/i3/config ~/Dokumenty/linux/Linux/md/.config/i3/
+cp -r ~/.config/i3/ ~/Dokumenty/linux/Linux/md/.config/
 cp ~/.config/i3status/config ~/Dokumenty/linux/Linux/md/.config/i3status/
 cp ~/.startup_zsh/* ~/Dokumenty/linux/Linux/md/.startup_zsh
 cp ~/.tmux.conf ~/Dokumenty/linux/Linux/md/
