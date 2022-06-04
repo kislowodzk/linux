d6f84c58e8bcb1162a9e674d71d81014b5d09927
kislowodzk <kislowodzk@gmail.com>
2022-06-04 17:06:49 +0200

    pos ringtone

---
+++ Linux/md/skrypty/pomodoro_sound.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+unhide_cursor() {
+    printf '\e[?25h'
+}
+trap 'unhide_cursor ; exit' INT
+
+# Hide the cursor (there is probably a much better way to do this)
+printf '\e[?25l'
+clear
+for i in `seq $(($1*60)) -1 1`
+do
+    x=$((i/60))
+    y=$((i%60))
+    printf '\033[;H'
+    echo "$x m $y s " | toilet
+    sleep 1
+done
+unhide_cursor
+clear
+mpv --no-video ~/skrypty/Alarm-clock-bell-ringing-sound-effect.mp3
+cmatrix
+
