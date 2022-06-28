4515354da69e83a532b51655eaa891f05e9ee504
kislowodzk <kislowodzk@gmail.com>
2022-06-28 21:18:56 +0200

    Update arandr_i3.sh

--- Linux/md/skrypty/arandr_i3.sh
+++ Linux/md/skrypty/arandr_i3.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xrandr --output LVDS-1 --primary --mode 1366x768 --pos 0x618 --rotate normal --output VGA-1 --mode 1920x1080 --pos 1366x0 --rotate normal --output HDMI-1 --off --output DP-1 --off
+xrandr --output LVDS-1 --primary --mode 1366x768 --pos 0x753 --rotate normal --output VGA-1 --mode 1920x1080 --pos 1366x0 --rotate normal --output HDMI-1 --off --output DP-1 --off
 i3-msg restart
