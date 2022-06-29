fc7e8d299a920bf0dabae6925e5ce943e3eefac1
kislowodzk <kislowodzk@gmail.com>
2022-06-29 22:30:16 +0200

    feh

--- Linux/md/skrypty/arandr_i3.sh
+++ Linux/md/skrypty/arandr_i3.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xrandr --output LVDS-1 --primary --mode 1366x768 --pos 0x753 --rotate normal --output VGA-1 --mode 1920x1080 --pos 1366x0 --rotate normal --output HDMI-1 --off --output DP-1 --off
-i3-msg restart
+xrandr --output LVDS-1 --mode 1366x768 --pos 0x753 --rotate normal --output VGA-1 --primary --mode 1920x1080 --pos 1366x0 --rotate normal --output HDMI-1 --off --output DP-1 --off
+exec --no-startup-id "feh --bg-scale ~/.tapeta/tapeta_gruvbox_orange.jpg"
--- Linux/md/skrypty/gif.sh
+++ Linux/md/skrypty/gif.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# DIR="$HOME/skrypty/obrazki/"
 DIR="$HOME/skrypty/gify/"
 LOGO="$(find "$DIR"/*.gif | shuf -n1)"
+# LOGO="$(find "$DIR"/* | shuf -n1)"
 kitty +kitten icat --align left $LOGO
 echo
