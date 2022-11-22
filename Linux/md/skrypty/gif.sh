fffa3d996d2a3f96317f2108ce652c78e8bd06a1
kislowodzk <kislowodzk@gmail.com>
2022-11-22 01:32:51 +0100

    iuhag

--- Linux/md/skrypty/gif.sh
+++ Linux/md/skrypty/gif.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # DIR="$HOME/skrypty/obrazki/"
-DIR="$HOME/skrypty/gify/"
+DIR="$HOME/skrypty/gify/witaj"
 LOGO="$(find "$DIR"/*.gif | shuf -n1)"
 # LOGO="$(find "$DIR"/* | shuf -n1)"
 kitty +kitten icat --align left $LOGO
