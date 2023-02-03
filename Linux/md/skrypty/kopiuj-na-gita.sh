cbeaaa54e162e2400de7db21b862a1b7b4fe4d12
kislowodzk <kislowodzk@gmail.com>
2023-02-03 14:14:23 +0100

    LaTeX Font

--- Linux/md/skrypty/kopiuj-na-gita.sh
+++ Linux/md/skrypty/kopiuj-na-gita.sh
@@ -11,7 +11,7 @@ cp -r ~/.config/qutebrowser/ ~/Dokumenty/linux/Linux/md/.config/
 cp -r ~/.config/cava/ ~/Dokumenty/linux/Linux/md/.config/
 cp ~/.config/ranger/rc.conf ~/Dokumenty/linux/Linux/md/.config/ranger
 cp ~/.zshrc ~/Dokumenty/linux/Linux/md/
-cp ~/.fonts/* ~/Dokumenty/linux/Linux/md/.fonts
+cp -r ~/.fonts/* ~/Dokumenty/linux/Linux/md/.fonts
 cp -r ~/.config/i3/ ~/Dokumenty/linux/Linux/md/.config/
 cp ~/.config/okularrc ~/Dokumenty/linux/Linux/md/.config/
 cp ~/.config/okularpartrc ~/Dokumenty/linux/Linux/md/.config/
