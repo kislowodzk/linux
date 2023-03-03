2d01bb3c081952108f81b9dc62a3b8a18723d9aa
kislowodzk <kislowodzk@gmail.com>
2023-03-03 01:26:02 +0100

    Update shutdown.sh

--- Linux/md/skrypty/shutdown.sh
+++ Linux/md/skrypty/shutdown.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-read -n1 -p "What to do? (k)ill, (r)eboot, (e)xit i3 or (l)og out?" doit 
+read -n1 -p "What to do? (k)ill, (r)eboot, (s)leep/hibernate or (l)og out?" doit 
 case $doit in  
   k|K) shutdown now ;; 
   r|R) reboot ;; 
-  e|E) i3-msg exit ;;
+  # e|E) i3-msg exit ;;
   l|L) gdmflexiserver ;; 
   s|S) systemctl suspend ;;
   *) echo "W klawisz nie umiesz trafic?" ;; 
