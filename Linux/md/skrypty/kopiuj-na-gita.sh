bd678b59edab3d5bf8d29d6d3757b71d9a4de05d
kislowodzk <kislowodzk@gmail.com>
2023-11-22 14:39:23 +0100

    a

--- Linux/md/skrypty/kopiuj-na-gita.sh
+++ Linux/md/skrypty/kopiuj-na-gita.sh
@@ -8,16 +8,15 @@ cp ~/.bashrc ~/Dokumenty/linux/Linux/md/
 # cp -r ~/.config/nvim/ ~/Dokumenty/linux/Linux/md/.config/
 cp -r ~/.config/xfce4/ ~/Dokumenty/linux/Linux/md/.config/
 cp -r ~/.config/qutebrowser/ ~/Dokumenty/linux/Linux/md/.config/
-cp -r ~/.config/cava/ ~/Dokumenty/linux/Linux/md/.config/
+# cp -r ~/.config/cava/ ~/Dokumenty/linux/Linux/md/.config/
 cp ~/.config/ranger/rc.conf ~/Dokumenty/linux/Linux/md/.config/ranger
 cp ~/.zshrc ~/Dokumenty/linux/Linux/md/
 cp -r ~/.fonts/* ~/Dokumenty/linux/Linux/md/.fonts
 # cp -r ~/.config/i3/ ~/Dokumenty/linux/Linux/md/.config/
-cp ~/.config/okularrc ~/Dokumenty/linux/Linux/md/.config/
-cp ~/.config/okularpartrc ~/Dokumenty/linux/Linux/md/.config/
+# cp ~/.config/okularrc ~/Dokumenty/linux/Linux/md/.config/
+# cp ~/.config/okularpartrc ~/Dokumenty/linux/Linux/md/.config/
 # cp ~/.config/i3status/config ~/Dokumenty/linux/Linux/md/.config/i3status/
 # cp ~/.startup_zsh/* ~/Dokumenty/linux/Linux/md/.startup_zsh
 # cp -r ~/.tapeta/* ~/Dokumenty/linux/Linux/md/.tapeta
-cp ~/.tmux.conf ~/Dokumenty/linux/Linux/md/
-
+# cp ~/.tmux.conf ~/Dokumenty/linux/Linux/md/
 cp -r ~/skrypty/ ~/Dokumenty/linux/Linux/md/
